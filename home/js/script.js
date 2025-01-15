412dc998e7fafcf2a1b3516febd72b67c3952da1
Clarice Benoman <156344601+apccvbenoman@users.noreply.github.com>
2025-01-15 18:22:56 +0800

    update

--- home/js/script.js
+++ home/js/script.js
@@ -1,36 +1,20 @@
-const { createApp } = Vue;
+       new Vue({
+            el: '#app',
+            data: {
+                images: [
+                    "https://scontent.xx.fbcdn.net/v/t1.15752-9/473007815_914812974174734_2264539277980305085_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=109&ccb=1-7&_nc_sid=0024fc&_nc_ohc=ZgahcL6whHgQ7kNvgFS8esc&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gFc25jJyhgE2ZI8ImLW_0dAJsWqSvBpRxVmIZTg6r7EOQ&oe=67ACB07C",
+                    "https://scontent.xx.fbcdn.net/v/t1.15752-9/473121470_503519732758484_7169031855201699330_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=108&ccb=1-7&_nc_sid=0024fc&_nc_ohc=_mJDblVVexoQ7kNvgEhZwMn&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gGbkLLwQO8TIQdhnrZLp_cGNdTVy8aSoH6fGXB707F1iw&oe=67ACC85C",
+                    "https://scontent.xx.fbcdn.net/v/t1.15752-9/467852735_593737746700096_4051191076940779528_n.jpg?stp=dst-jpg_p480x480_tt6&_nc_cat=111&ccb=1-7&_nc_sid=0024fc&_nc_ohc=OZ182LbCuHQQ7kNvgHfhgRu&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gEOHYbFHFuHflokNStfoJcgGqx_23X_ZAAu_jaXXjpL3g&oe=67ADF8F3",
+                    "https://scontent.xx.fbcdn.net/v/t1.15752-9/330376735_1353440542171658_6593644632786779640_n.jpg?stp=dst-jpg_s640x640_tt6&_nc_cat=111&ccb=1-7&_nc_sid=0024fc&_nc_ohc=kniDc8MJPAkQ7kNvgGzMCZC&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gH3tuAw5Ohw-jCtsCO6JFU5fdSYWsEBgPJUDMi5_C8afg&oe=67ADCDDD"
+                ]
+            },
+            methods: {
+                viewImage(image) {
+                    window.open(image, "_blank");
+                }
+            }
+        });
 
-createApp({
-  data() {
-    return {
-      images: [
-        {
-          id: 1,
-          src: "https://scontent.xx.fbcdn.net/v/t1.15752-9/467478354_1106522184430313_4855955892637414384_n.jpg",
-          alt: "Gallery Image 1",
-        },
-        {
-          id: 2,
-          src: "https://scontent.fmnl5-2.fna.fbcdn.net/v/t1.15752-9/467752633_1824689381653523_8277619900379541539_n.jpg",
-          alt: "Gallery Image 2",
-        },
-        {
-          id: 3,
-          src: "https://scontent.xx.fbcdn.net/v/t1.15752-9/330376735_1353440542171658_6593644632786779640_n.jpg",
-          alt: "Gallery Image 3",
-        },
-        {
-          id: 4,
-          src: "https://scontent.xx.fbcdn.net/v/t1.15752-9/473007815_914812974174734_2264539277980305085_n.jpg",
-          alt: "Gallery Image 4",
-        },
-        {
-          id: 5,
-          src: "https://scontent.xx.fbcdn.net/v/t1.15752-9/467852735_593737746700096_4051191076940779528_n.jpg",
-          alt: "Gallery Image 5",
-        },
-      ],
-    };
-  },
-}).mount("#app");
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 
